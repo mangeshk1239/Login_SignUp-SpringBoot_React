2e6d6fa2b52eb6c7ff6ecd32f9eff9b69eff9a36
Mangesh Kamble <mangeshkamble1239@gmail.com>
2023-11-20 19:06:38 +0530

    few changes

--- backend/src/main/java/com/webapp/authentication/controller/UserController.java
+++ backend/src/main/java/com/webapp/authentication/controller/UserController.java
@@ -3,6 +3,7 @@ package com.webapp.authentication.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,10 +17,11 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     public void createUser(@RequestBody UserEntity userData) {
         String password = userData.getPassword();
+        System.out.println(password);
         
-        userService.create(userData);
+        // userService.create(userData);
     }
 }
